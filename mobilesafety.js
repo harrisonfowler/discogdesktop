515eef54d9efec06cac72bb227975c88b9120639
harrisonfowler <harrisonxfowler@gmail.com>
2018-10-31 11:16:26 +0000

    Add files via upload

--- mobilesafety.js
+++ mobilesafety.js
@@ -3,7 +3,15 @@ $(document).ready(function() {
 	/* If mobile browser, prevent click on parent nav item from redirecting to URL */
 	if(is_touch_device()) {	
 
-		$('#mainmenu li > ul').each(function (index, elem) {
+		$('#dropup-content').each(function (index, elem) {
+			
+			/* OR Option 2: Use this to keep the href on the <a> intact but prevent the default action */
+			$(elem).prev('a').click(function(event) {
+  				event.preventDefault();
+			});
+		});
+		$('#dropup-content2').each(function (index, elem) {
+			
 			/* OR Option 2: Use this to keep the href on the <a> intact but prevent the default action */
 			$(elem).prev('a').click(function(event) {
   				event.preventDefault();
